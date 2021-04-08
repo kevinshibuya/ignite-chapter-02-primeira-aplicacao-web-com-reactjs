73912eb5f3826a44894ddcbc1da94d6139af663d
Kevin Shibuya <kevinshibuya@hotmail.com>
2021-04-08 14:51:50 -0300

    styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`
+
 export function App() {
   return (
     <div className="App">
-      <div>Hello world</div>    
+      <Title>Hello world</Title>
     </div>
   );
 }
